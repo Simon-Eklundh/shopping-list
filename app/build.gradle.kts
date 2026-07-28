ea95da870b6f2bd7d62c694cbcb3e69f56d2e153
Simon Eklundh <git@oakgrove.site>
2026-07-28 15:46:01 +0200

    correctly count list items on homepage

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,7 +16,7 @@ android {
         //noinspection OldTargetApi
         targetSdk = 36
         versionCode = 1
-        versionName = "1.3"
+        versionName = "1.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
